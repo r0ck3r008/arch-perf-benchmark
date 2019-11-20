7e63f66303cdd9cba2d0ecc56e4e4b2a96a27fbb
Brendan John <brendanjohn@ufl.edu>
2019-11-20 06:40:54 +0000

    Added implementations. Compiles. Have not tested functions yet, and generally I'm terrible at C, but will test them out tomorrow.

--- src/samples/ppm_encrypt.c
+++ src/samples/ppm_encrypt.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<stdint.h>
 #include<string.h>
+#include <math.h>
 
 typedef struct {
     unsigned char r,g,b;
@@ -152,61 +153,141 @@ void swap(unsigned char*a, unsigned char*b) {
 	*b = temp;
 }
 
-//Output M is a 256 byte state vector, initialized by K with 128 byte length(TODO: decide on length, picked 128 bytes for now)
-//256 one byte entries
+//Output M is a 256 byte state vector, initialized by K with 128 byte length
 void KSA(char *K,unsigned char *M){
 	char *T; //potential error initializing without length known
 	for (int k = 0;k < 256;k++){
 		M[k] = k;
-		T[k] = K[k % 128] //key has length of 128 bytes
+		T[k] = K[k % 128]; //key has length of 128 bytes
 	}
 	
 	int j = 0;
 	for (int k = 0;k < 256;k++){
 		j = (j + M[k] + T[k]) % 256;
-		swap(&M[i],&M[j]); // this should swap them within M
+		swap(&M[k],&M[j]); // this should swap them within M
 	}
 }
 
-void PRGA(unsigned char*M,char *pt, unsigned char *ct){
+void PRGA(Image * img, unsigned char*M){ 
+	unsigned int w = getImgWidth(img);
+    unsigned int h = getImgHeight(img);
+	 
 	int k,j = 0;
 	
-	//loop until done with pt 
-	for (size_t i = 0; len = strlen(pt); i < len; i++){
+	Pixel * p = img->data;
+	for (int i = 0;i < w*h;i++){
+		
 		k = (k+1) % 256;
 		j = (j + M[k]) % 256;
 		swap(&M[k],&M[j]);
 		
-		//apply key as xor with pt byte
-		ct[i] = ((M[k]+m[j]) % 256) ^ pt[i];
+		//apply key as xor with just red channel for now.
+		p[i].r = ((M[k]+M[j]) % 256) ^ p[i].r;
 	}
 }
 
 void encrypt_RC4(Image *img){
-	//TODO: 
-	// Convert img from 2D image to 1D stream, perhaps as 1D stream of PPMPixel? Same way negativer color code above loops through as 1D, each pixel has 3 bytes	(RGB)
-	// define an the key K, and init empty M 
-	// then call KSA to fill M, then call PRGA to encrypt. 
-}
-
-void decrypt_RC4(Image *img){
-	//TODO: same thing as encrypt 
-}
-
-void encrypt_Vigenere(Image *img){
+	//define key and M to fill  
+	char *K;
+	unsigned char *M; //TODO: see how they init S in example code 
 	
+	strncpy(K,"\xee\xf7\x62\x66\x74\x48\xb9\x43\x4d\xaf\xe5\xf7\xb2\x68\xf5\xec\xf6\x53\x46\xcd\x0e\x54\xcf\xb9\x9f\x9d\x6f\xca\x45\xec\x91\x15\x87\x12\x3f\x2c\xcc\xbc\xaf\x51\x54\xb8\x85\x86\xfb\x48\x3d\x6d\xf0\xad\xb1\xd4\x5f\x5c\x65\x0a\xc7\x14\x16\x03\xd2\x15\x5d\x90\xb9\x2b\x03\x06\xc4\xa2\xf7\xee\xce\x81\xd0\x25\x40\xec\xfc\xc7\xa9\x7e\xec\x28\x58\x02\xc8\xd1\x9c\x88\xfc\x49\x30\x1e\xb8\xce\xf4\x11\x62\x85\x75\x43\x2b\x3d\x3e\x4f\x95\xac\x43\x16\x36\xdc\xec\x96\xff\xf8\xe6\xbc\xa2\xb4\xd7\x5e\xcd\xad\xd8\x8a\x28\xd1",128);
+	
+	KSA(K,M);
+	
+	PRGA(img,M);
+}
+//same as encrypt 
+void decrypt_RC4(Image *img){ 
+	//define key and M to fill  
+	char *K;
+	unsigned char *M; //TODO: see how they init S in example code 
+	
+	strncpy(K,"\xee\xf7\x62\x66\x74\x48\xb9\x43\x4d\xaf\xe5\xf7\xb2\x68\xf5\xec\xf6\x53\x46\xcd\x0e\x54\xcf\xb9\x9f\x9d\x6f\xca\x45\xec\x91\x15\x87\x12\x3f\x2c\xcc\xbc\xaf\x51\x54\xb8\x85\x86\xfb\x48\x3d\x6d\xf0\xad\xb1\xd4\x5f\x5c\x65\x0a\xc7\x14\x16\x03\xd2\x15\x5d\x90\xb9\x2b\x03\x06\xc4\xa2\xf7\xee\xce\x81\xd0\x25\x40\xec\xfc\xc7\xa9\x7e\xec\x28\x58\x02\xc8\xd1\x9c\x88\xfc\x49\x30\x1e\xb8\xce\xf4\x11\x62\x85\x75\x43\x2b\x3d\x3e\x4f\x95\xac\x43\x16\x36\xdc\xec\x96\xff\xf8\xe6\xbc\xa2\xb4\xd7\x5e\xcd\xad\xd8\x8a\x28\xd1",128);
+	
+	KSA(K,M);
+	
+	PRGA(img,M);
 }
 
-void decrypt_Vigenere(Image *img){
+Image * encrypt_Vigenere(Image *img, char* K){
+	//first init empty target image 
+	unsigned int w = getImgWidth(img);
+    unsigned int h = getImgHeight(img);
+	Image *img_cypher = new_image(w,h);
 	
+	Pixel * p_ct = img_cypher->data;
+	Pixel * p_pt = img->data;
+	for (int i = 0;i<w;i++){
+		for (int j = 0;j<h;j++){
+			p_ct[i + j*w].r = (K[j] + p_pt[i + j*w].r) % 256;
+		}
+	}
+	
+	
+	return img_cypher;
 }
 
-void encrypt_Chirikov(Image *img){
+Image * decrypt_Vigenere(Image *img, char* K){
+	//first init empty target image 
+	unsigned int w = getImgWidth(img);
+    unsigned int h = getImgHeight(img);
+	Image *img_plain = new_image(w,h);
 	
+	Pixel * p_pt = img_plain->data;
+	Pixel * p_ct = img->data;
+	for (int i = 0;i<w;i++){
+		for (int j = 0;j<h;j++){
+			p_pt[i + j*w].r = (K[j] - p_ct[i + j*w].r) % 256;
+		}
+	}
+	
+	
+	return img_plain;
 }
 
-void decrypt_Chirikov(Image *img){
+Image * encrypt_Chirikov(Image *img, int K){
+	unsigned int w = getImgWidth(img);
+    unsigned int h = getImgHeight(img);
+	Image *img_cypher = new_image(w,h);
 	
+	int x_prime, y_prime;
+	
+	Pixel * p_ct = img_cypher->data;
+	Pixel * p_pt = img->data;
+	for (int i = 0;i<w;i++){
+		for (int j = 0;j<h;j++){
+			if (i+j!= 0) {
+				x_prime = (i-1+j-1) % h;
+				y_prime = (unsigned int ) (floor(j-1+K*sin(2*M_PI*x_prime/h))) % h;
+				p_ct[x_prime + y_prime*w].r = p_pt[i + j*w].r;
+			}
+		}
+	}
+	
+	return img_cypher;
+}
+
+Image * decrypt_Chirikov(Image *img, int K){
+	unsigned int w = getImgWidth(img);
+    unsigned int h = getImgHeight(img);
+	Image *img_plain = new_image(w,h);
+	
+	int x_prime, y_prime;
+	
+	Pixel * p_pt = img_plain->data;
+	Pixel * p_ct = img->data;
+	for (int i = 0;i<w;i++){
+		for (int j = 0;j<h;j++){
+			if (i+j!= 0) {
+				x_prime = (unsigned int ) (floor(i-j+K*sin(2*M_PI*i/h))) % h;
+				y_prime = (unsigned int ) (floor(j-K*sin(2*M_PI*i/h))) % h;
+				p_pt[x_prime + y_prime*w].r = p_ct[i + j*w].r;
+			}
+		}
+	}
+	
+	return img_plain;
 }
 
 #define max(a,b) ((a>b)?(a):(b))
@@ -265,10 +346,10 @@ int main()
 
 	//TODO: switch this to an encoding function
 	//apply emboss filter
-	Image * result = emboss_image(img);
+	//Image * result = emboss_image(img);
 
 	//write processed image to file
-	write_PPM("result.ppm", result);
+	write_PPM("result.ppm", img);
 
 	//printf("Press any key to close.");
     //getchar();
