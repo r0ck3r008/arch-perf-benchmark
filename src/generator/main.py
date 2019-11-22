1cad3da1b2487abb12d855236e1e6771ab38f253
r0ck3r008 <naman.arora@tutanota.com>
2019-11-22 09:14:52 -0500

    added destination paths for the arguments, main now calls stream.parse_file_type

--- src/generator/main.py
+++ src/generator/main.py
@@ -6,7 +6,9 @@ stream=import_module('stream', '.')
 if __name__=='__main__':
     parser=ArgumentParser()
     parser.add_argument('-s', '--stream', required=True, metavar='',
-                help='The stream to read from (image/video/network)')
+                dest='sfile', help='The stream to read from (image/video/network/Webcam)')
     parser.add_argument('-f', '--fifo', required=True, metavar='',
-                help='The FIFO to create/use to push bit stream')
+                dest='fifo', help='The FIFO to create/use for pushing bit stream')
     args=parser.parse_args()
+
+    stream.parse_file_type(args.sfile)
