8aadb8b756657ae1aaea026b5c05df78f6832de7
r0ck3r008 <naman.arora@tutanota.com>
2019-11-23 01:55:58 -0500

    successful test of writeing image to a file, pending reading test in rv8

--- src/generator/stream.py
+++ src/generator/stream.py
@@ -3,29 +3,41 @@ import cv2
 
 io=import_module('io', '.')
 
-def parse_file_type(s_file):
-    if '.jpg' in s_file or '.png' in s_file:
-        to_stream_img(s_file)
-    elif '.mp4' in s_file or 'http' in s_file:
-        to_stream_vid(s_file)
-    else:
-        to_stream_vid(int(s_file))
-
 #TODO
-#handle networked stream, form a packet header to pass numpy.ndarray to the pipe
-def to_stream_vid(s_file):
+#form a packet header to pass numpy.ndarray to the pipe
+def to_stream(f, frame):
+    for row in frame:
+        for col in row:
+            for val in col:
+                f.write('{};'.format(val))
+            f.write("\n")
+
+def to_ndarray_vid(s_file):
     cap_obj=cv2.VideoCapture(s_file)
+    f=open('write_file', 'w')
     while True:
         #to numpy.ndarray
         ret, frame=cap_obj.read()
         if ret==False:
             break
-        print(type(frame))
+        to_stream(f, frame)
 
     cap_obj.release()
     cv2.destroyAllWindows()
+    f.close()
 
-def to_stream_img(s_file):
+def to_ndarray_img(s_file):
     #to numpy.ndarray
-    pxl_array=cv2.imread(s_file)
-    print(pxl_array)
+    frame=cv2.imread(s_file)
+    f=open('write_file', 'w')
+    to_stream(f, frame)
+    f.close()
+
+def parse_file_type(s_file):
+    if '.jpg' in s_file or '.png' in s_file:
+        to_ndarray_img(s_file)
+    elif '.mp4' in s_file or 'http' in s_file:
+        to_ndarray_vid(s_file)
+    else:
+        to_ndarray_vid(int(s_file))
+
