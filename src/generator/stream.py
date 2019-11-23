dd9deb8c87f7fdd64bd625c77e89b6c0f9f691e5
r0ck3r008 <naman.arora@tutanota.com>
2019-11-23 01:00:12 -0500

    working network stream from IP Webcam

--- src/generator/stream.py
+++ src/generator/stream.py
@@ -1,17 +1,14 @@
-from matplotlib.image import imread
 from importlib import import_module
 import cv2
 
 io=import_module('io', '.')
 
 def parse_file_type(s_file):
-    splt=s_file.split('.')
-    if len(splt)==2:
-        if splt[1]=='jpg' or splt=='png':
-            to_stream_img(s_file)
-        elif splt[1]=='mp4':
-            to_stream_vid(s_file)
-    elif len(splt)==0:
+    if '.jpg' in s_file or '.png' in s_file:
+        to_stream_img(s_file)
+    elif '.mp4' in s_file or 'http' in s_file:
+        to_stream_vid(s_file)
+    else:
         to_stream_vid(int(s_file))
 
 #TODO
@@ -20,17 +17,15 @@ def to_stream_vid(s_file):
     cap_obj=cv2.VideoCapture(s_file)
     while True:
         #to numpy.ndarray
-        _, frame=cap_obj.read()
-        print(type(frame))
-
-        if cv2.waitKey(1) & 0xFF==ord('q'):
+        ret, frame=cap_obj.read()
+        if ret==False:
             break
+        print(type(frame))
 
     cap_obj.release()
     cv2.destroyAllWindows()
 
 def to_stream_img(s_file):
     #to numpy.ndarray
-    pxl_array=imread(s_file)
-    print(type(pxl_array))
-
+    pxl_array=cv2.imread(s_file)
+    print(pxl_array)
