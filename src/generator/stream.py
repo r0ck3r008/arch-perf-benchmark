cd1d75386f4a9e6d36f0fc53acc0327e8fe4ee8e
r0ck3r008 <naman.arora@tutanota.com>
2019-11-26 22:45:38 -0500

    added a sub processing implementation to process each frame with feature to open its own pipe

--- src/generator/stream.py
+++ src/generator/stream.py
@@ -1,7 +1,16 @@
 from importlib import import_module
 import cv2
+from multiprocessing import Process
+from libnacl import randombytes_uniform as ru
+from os import remove
 
 io_helper=import_module('io_helper', '.')
+
+def process_frame(frame):
+    fname='/tmp/pipe_{}'.format(ru(1000000))
+    io_helper.open_fifo(fname)
+    io_helper.write(frame, fname)
+    remove(fname)
 
 #TODO
 #form a packet header to pass numpy.ndarray to the pipe
@@ -12,7 +21,9 @@ def to_ndarray_vid(s_file):
         ret, frame=cap_obj.read()
         if ret==False:
             break
-        io_helper.write(frame, fname)
+        p=Process(target=process_frame, args=[frame,])
+        processes.append(p)
+        p.start()
 
     cap_obj.release()
     cv2.destroyAllWindows()
@@ -20,15 +31,17 @@ def to_ndarray_vid(s_file):
 def to_ndarray_img(s_file):
     #to numpy.ndarray
     frame=cv2.imread(s_file)
-    io_helper.write(frame, fname)
 
-def parse_file_type(s_file, fifo):
-    global fname
-    fname=fifo
+def parse_file_type(s_file):
+    global processes
+    processes=[]
     if '.jpg' in s_file or '.png' in s_file:
         to_ndarray_img(s_file)
     elif '.mp4' in s_file or 'http' in s_file:
         to_ndarray_vid(s_file)
     else:
         to_ndarray_vid(int(s_file))
+
+    for process in processes:
+        process.join()
 
