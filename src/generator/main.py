8649baf32fa064e0e54a100372392318d0a46194
r0ck3r008 <naman.arora@tutanota.com>
2019-11-26 22:44:54 -0500

    the implementation of named pipe is shifted to sub process level, so global pipe not required

--- src/generator/main.py
+++ src/generator/main.py
@@ -8,9 +8,6 @@ if __name__=='__main__':
     parser=ArgumentParser()
     parser.add_argument('-s', '--stream', required=True, metavar='',
                 dest='sfile', help='The stream to read from (image/video/network/Webcam)')
-    parser.add_argument('-f', '--fifo', required=True, metavar='',
-                dest='fifo', help='The FIFO to create/use for pushing bit stream')
     args=parser.parse_args()
 
-    io_helper.open_fifo(args.fifo)
-    stream.parse_file_type(args.sfile, args.fifo)
+    stream.parse_file_type(args.sfile)
