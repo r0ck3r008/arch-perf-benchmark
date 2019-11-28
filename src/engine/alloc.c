bb2040972ba359d75655836dd9a35fec1522cf0a
r0ck3r008 <naman.arora@tutanota.com>
2019-11-27 23:53:08 -0500

    added image allocation

--- src/engine/alloc.c
+++ src/engine/alloc.c
@@ -4,7 +4,24 @@
 #include<unistd.h>
 
 #include"alloc.h"
-//#include"global_defs.h"
+
+struct image *alloc_image(int size_h, int size_w)
+{
+	struct image *im=alloc("struct image", 1);
+
+	im->width=size_w;
+	im->height=size_h;
+
+	im->data=alloc("struct pixel", size_h*size_w);
+
+	return im;
+}
+
+void dealloc_image(struct image *im)
+{
+	dealloc("struct pixel", im->height*im->width, im->data);
+	dealloc("struct image", 1, im);
+}
 
 void *alloc(char *type, int size)
 {
@@ -13,6 +30,12 @@ void *alloc(char *type, int size)
 	if(!strcmp(type, "char")){
 		ret=malloc(sizeof(char)*size);
 		explicit_bzero(ret, sizeof(char)*size);
+	} else if(!strcmp(type, "struct pixel")){
+		ret=malloc(sizeof(struct pixel)*size);
+		explicit_bzero(ret, sizeof(struct pixel)*size);
+	} else if(!strcmp(type, "struct image")){
+		ret=malloc(sizeof(struct image)*size);
+		explicit_bzero(ret, sizeof(struct image)*size);
 	}
 
 	if(ret==NULL){
@@ -26,6 +49,10 @@ void dealloc(char *type, int size, void *buf)
 {
 	if(!strcmp(type, "char")){
 		explicit_bzero(buf, sizeof(char)*size);
+	} else if(!strcmp(type, "struct pixel")){
+		explicit_bzero(buf, sizeof(struct pixel)*size);
+	} else if(!strcmp(type, "struct image")){
+		explicit_bzero(buf, sizeof(struct image)*size);
 	}
 
 	free(buf);
--- src/engine/alloc.h
+++ src/engine/alloc.h
@@ -1,6 +1,10 @@
 #ifndef ALLOC_H
 #define ALLOC_H
 
+#include"global_defs.h"
+
+struct image *alloc_image(int, int);
+void dealloc_image(struct image *);
 void *alloc(char *, int);
 void dealloc(char *, int, void *);
 
