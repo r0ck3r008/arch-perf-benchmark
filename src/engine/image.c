c5fdacc11feab88a20dc074de20bb0a01681bcb4
r0ck3r008 <naman.arora@tutanota.com>
2019-11-27 23:54:26 -0500

    added image reader function

---
+++ src/engine/image.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include"alloc.h"
+
+struct image *make_image(char *data, int size_h, int size_w)
+{
+	struct image *im=alloc_image(size_h, size_w);
+	char *a=strtok(data, ";");
+	for(int i=0; a!=NULL; i++){
+		int buf[3];
+		for(int j=0; j<3; j++){
+			buf[j]=strtol(a, NULL, 10);
+			a=strtok(NULL, ";");
+		}
+		im->data[i].r=buf[0];
+		im->data[i].g=buf[1];
+		im->data[i].b=buf[2];
+	}
+
+	printf("[!]Image received\n");
+	return im;
+}
---
+++ src/engine/image.h
@@ -0,0 +1,8 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include"global_defs.h"
+
+struct image *make_image(char *, int, int);
+
+#endif
