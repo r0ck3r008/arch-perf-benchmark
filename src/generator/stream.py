e38d8d27776fd935d430cf7127b057e03f4ddb40
r0ck3r008 <naman.arora@tutanota.com>
2019-11-29 15:29:47 -0500

    added subprocess call to engine for encryption, also added 60 times looping over is stream is image

--- src/generator/stream.py
+++ src/generator/stream.py
@@ -1,19 +1,23 @@
 from importlib import import_module
 import cv2
 from multiprocessing import Process
+from threading import Thread
 from libnacl import randombytes_uniform as ru
 from os import remove
+from subprocess import run, PIPE
+from time import sleep
 
 io_helper=import_module('io_helper', '.')
 
 def process_frame(frame):
     fname='/tmp/pipe_{}'.format(ru(1000000))
     io_helper.open_fifo(fname)
-    io_helper.write(frame, fname)
+    t=Thread(target=io_helper.write, args=[frame, fname]).start()
+    sleep(10)
+    p=run('./engine/engine {} 0'.format(fname), shell=True, stdout=PIPE, text=True)
+    print(p.stdout)
     remove(fname)
 
-#TODO
-#form a packet header to pass numpy.ndarray to the pipe
 def to_ndarray_vid(s_file):
     cap_obj=cv2.VideoCapture(s_file)
     while True:
@@ -30,8 +34,11 @@ def to_ndarray_vid(s_file):
 
 def to_ndarray_img(s_file):
     #to numpy.ndarray
-    frame=cv2.imread(s_file)
-    process_frame(frame)
+    for i in range(60):
+        frame=cv2.imread(s_file)
+        p=Process(target=process_frame, args=[frame,])
+        processes.append(p)
+        p.start()
 
 def parse_file_type(s_file):
     global processes
