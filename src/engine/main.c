472dc892fce5b6c814b026245df210c01194e1a1
r0ck3r008 <naman.arora@tutanota.com>
2019-11-29 13:31:45 -0500

    added a subroutine which dynamically calls which function to use for encryption

--- src/engine/main.c
+++ src/engine/main.c
@@ -6,9 +6,28 @@
 #include"utils/image.h"
 #include"utils/time_keeper.h"
 
-void fun_selector(int selector)
-{
+#include"algorithms/rc4.h"
+#include"algorithms/chaos.h"
+#include"algorithms/vigenere.h"
 
+struct image *(*fun_selector (int selector))(struct image *, char *)
+{
+	struct image *(*encrypt_fn)(struct image *, char *);
+	switch(selector){
+	case 0:
+		encrypt_fn=encrypt_rc4;
+		break;
+/*	case 1:
+		encrypt_fn=encrypt_chirikov;
+		break;*/
+	case 2:
+		encrypt_fn=encrypt_vigenere;
+		break;
+	default:
+		fprintf(stderr, "[-]Undefined algorithm\n");
+	}
+
+	return encrypt_fn;
 }
 
 int main(int argc, char *argv[])
@@ -18,6 +37,11 @@ int main(int argc, char *argv[])
 	char *data=read_from(fifo_fd);
 
 	struct image *im=make_image(data, 240, 360);
-	time_keeper(im, NULL);
+
+	struct image *(*fn)
+		(struct image *, char *)=fun_selector(strtol(
+							argv[1], NULL, 10));
+
+	time_keeper(im, fn);
 	printf("[!]Done!\n");
 }
