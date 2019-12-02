cca86819d73264a526927a32095e573154ffe2ec
bmj8778 <brendanjohn@ufl.edu>
2019-12-01 23:03:03 -0500

    Added std dev to the output.

--- src/generator/list_ops.py
+++ src/generator/list_ops.py
@@ -2,5 +2,8 @@ def list_ops(times):
     s=0.0
     for val in times:
         s+=val
-
-    print('Avg: {}, Min: {}, Max: {}'.format(s/len(times), min(times), max(times)))
+    mean = s/len(times)
+    std_dev = 0.0
+    for val in times:
+        std_dev += (val-mean)**2
+    print('Avg: {}, Std: {}, Min: {}, Max: {}'.format(mean, std_dev, min(times), max(times)))
